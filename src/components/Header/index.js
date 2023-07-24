99012e7b0bfe369117578d87b91cc2dd8612681d
Franciane <francianemenestrina@gmail.com>
2023-07-24 15:54:07 +0200

    starting project

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+const Header = () => {
+  return (
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <a
+            href="#"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign up
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import Header from "../components/Header";
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="flex justify-center mt-20 text-8xl">Ewald Engenharia</h1>
+    </div>
+  );
+};
+
+export default Index;
