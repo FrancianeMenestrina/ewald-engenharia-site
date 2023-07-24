3c0533349ac90b32c00fa4e230bb24f380d9e809
Franciane <francianemenestrina@gmail.com>
2023-07-24 22:36:39 +0200

    starting hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import "./styles.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import WhatsAppIcon from "../components/WhatsAppIcon";
+import Hero from "../components/Hero";
 
 const Index = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
       <div className="flex items-center flex-1">
         <h1 className="flex justify-center mt-20 text-8xl flex-1 ">
           Ewald Engenharia
